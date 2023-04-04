65a9d4d3262cfdc72a5df5961985b60e58ca0e22
Filip Alin <alin.filip00@e-uvt.ro>
2023-04-04 16:58:58 +0300

    Gestionare Fire executie

--- gestionareTimp.pl
+++ gestionareTimp.pl
@@ -11,17 +11,30 @@ start_timer(Time, Action) :-
     assert(fireExecutie([ThreadId|Lista])).
 
 stop_timer([]).
-stop_timer([L|T]):-
-        thread_property(L, status(running)),
-        thread_signal(L, abort),
-        stop_timer(T),!.
-stop_timer([_|T]):-
-        stop_timer(T),!.
+stop_timer([Thread|Threads]):-
+        (   thread_property(Thread, status(running))
+    ->  thread_signal(Thread, abort),
+        fireExecutie(Lista),
+        eliminareFir(Thread,Lista,ListaNoua),
+        retract(fireExecutie(_)),
+        assert(fireExecutie(ListaNoua)),
+        stop_timer(Threads)
+    ;   (   thread_property(Thread, existence(true))
+        ->  true % thread exists, but is not running
+        ;   format('Thread ~w no longer exists.~n', [Thread])
+        ),
+        stop_timer(Threads)
+    ),!.
 
 timer_thread(Time, Action) :-
+    thread_self(ThreadId),
     sleep(Time),
     (   timer_thread_active
-    ->  call(Action)
+    ->  call(Action),
+    fireExecutie(Lista),
+    eliminareFir(ThreadId,Lista,ListaNoua),
+    retract(fireExecutie(_)),
+    assert(fireExecutie(ListaNoua))
     ;   true
     ).
 
@@ -34,3 +47,9 @@ timpRamas(Timpramas):-
         get_time(Ora),
         Timpramas is MomentFinalizare - Ora,
         stamp_date_time(Timpramas, _, 'local').
+
+eliminareFir(_,[],[]).
+eliminareFir(ThreadId,[ThreadId|Lista],ListaNoua):-
+        eliminareFir(ThreadId,Lista,ListaNoua),!.
+eliminareFir(ThreadId,[L|Lista],[L|ListaNoua]):-
+        eliminareFir(ThreadId,Lista,ListaNoua),!.
